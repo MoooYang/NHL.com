8573d2335e410c83b515bfb7b04a30d0c29c4807
MoooYang <34911355+MoooYang@users.noreply.github.com>
2018-11-29 19:24:28 -0500

    Done Done

--- __init__.py
+++ __init__.py
@@ -59,6 +59,7 @@ def teams():
                 ex3 =request.form['ex3']
                 select_stm = "select name, year, lgId, tmID, rank, G, W, L, T ,Pts, GF, GA from Team where "
                 where=''
+                groupby=''
                 data=[]
                 if year:
                     where = where + 'and year=%s '
@@ -70,13 +71,13 @@ def teams():
                     where = where + 'and name like %s '
                     data.append('%'+ tmName1 +'%')
                 if int(ex1):
-                    where = where + 'and W > %s '
+                    where = where + 'and W >= %s '
                     data.append(ex1)
                 if int(ex2):
-                    where = where + 'and L > %s '
+                    where = where + 'and L >= %s '
                     data.append(ex2)
                 if int(ex3):
-                    where = where + 'and T > %s '
+                    where = where + 'and T >= %s '
                     data.append(ex3)
                 title =['Team','Year','League','TeamID','Rank','Games','Wins','Losses','Ties','Pts','GF','GA']
             else:
@@ -85,20 +86,28 @@ def teams():
                 year=request.form['year2']
                 select_stm = "select * from team_game where "
                 where=''
+                groupby=''
                 data=[]
-                if year:
-                    where = where + 'and year=%s '
-                    data.append(year)
                 if tmName:
                     where = where + 'and name like %s '
                     data.append('%'+tmName+'%')
                 if tmNameP:
                     where = where + 'and pname like %s '
                     data.append('%'+tmNameP+'%')
-                title = ['Year','Home','Away','Wins','Losses','Ties']
+                if year:
+                    if year=='sum':
+                        select_stm='select name, pname, sum(Wins), sum(Losses), sum(Ties) from team_game where '
+                        groupby=' group by name, pname'
+                        title = ['Home','Away','Wins','Losses','Ties']
+                    else:
+                        where = where + 'and year=%s '
+                        data.append(year)
+                        title = ['Year','Home','Away','Wins','Losses','Ties']
+                else:
+                    title = ['Year','Home','Away','Wins','Losses','Ties']
             where=where[3:]
-            debug= select_stm+rep_with_value(where,data)
-            table=get_table(select_stm+where, data)
+            debug= select_stm+rep_with_value(where,data)+groupby
+            table=get_table(select_stm+where+groupby, data)
             teamdrop=get_dropoff('select distinct name from Team')
             years=get_dropoff('select distinct year from Games order by year')
             try:
@@ -108,11 +117,11 @@ def teams():
             return render_template("teams.html", table=table, years=years, debug=debug, year=year,teamdrop=teamdrop,title=title)
 
         else:
-            select_stm = "SELECT * FROM Games WHERE year =2008"
+            select_stm = "SELECT * FROM Games WHERE year =1927"
             table=get_table(select_stm)
             teamdrop=get_dropoff('select distinct name from Team')
             years=get_dropoff('select distinct year from Games order by year')
-            return render_template("teams.html", table=table, years=years, year=2008, lgID='NHL', tmID='',teamdrop=teamdrop)    
+            return render_template("teams.html", table=table, years=years, year=1927, lgID='NHL', tmID='',teamdrop=teamdrop)    
     except Exception as e:
         debug=e
         return render_template("teams.html", debug=debug)
@@ -127,45 +136,79 @@ def players():
             lgID=request.form['lgID']
             tmID=request.form['tmID']
             year=request.form['year']
+            pname=request.form['pname']
+            postion=request.form['position']
+            catch=request.form['catch']
+            age=request.form['age']
+            weight=request.form['weight']
+            height=request.form['height']
+            assists=request.form['assists']
+            pts=request.form['pts']
+            pim=request.form['pim']
+
+            where=''
             select_stm = "SELECT * FROM player_team WHERE "
             data=[]
             if year:
-                select_stm = select_stm + 'Syear=%s'
+                where = where + 'and Syear=%s '
                 data.append(year)
             if lgID!='ALL':
-                select_stm=select_stm + ' and lgID=%s'
+                where=where + 'and lgID=%s '
                 data.append(lgID)
             if tmID:
-                select_stm = select_stm + ' and tname like %s'
+                where = where + 'and tname like %s '
                 data.append('%'+tmID+'%')
-            if weight_from:
-                select_stm = select_stm + ' and weight >= %s'
-                data.append(weight_from)
-            if weight_to:
-                select_stm = select_stm + ' and weight <= %s'
-                data.append(weight_to)
-            if height_from:
-                select_stm = select_stm + ' and height >= %s'
-                data.append(height_from)
-            if height_to:
-                select_stm = select_stm + ' and height <= %s'
-                data.append(height_to)
+            if int(weight)!=125:
+                where = where + 'and weight >= %s '
+                data.append(weight)
+            if int(height)!=63:
+                where = where + 'and height >= %s '
+                data.append(height)
+            if pname:
+                where = where + 'and pname like %s '
+                data.append('%'+pname+'%')
+            if postion:
+                where = where + 'and pos= %s '
+                data.append(postion)
+            if catch:
+                where = where + 'and catch= %s '
+                data.append(catch)
+            if int(age)!=51:
+                where = where + 'and AgebySeason <= %s '
+                data.append(age)
+            if int(assists):
+                where = where + 'and Assists >= %s '
+                data.append(assists)
+            if int(pim):
+                where = where + 'and PIM >= %s '
+                data.append(pim)
+            if int(pts):
+                where = where + 'and Pts >= %s '
+                data.append(pts)
+
+        
 
 
-
-            debug=select_stm
+            where=where[3:]
+            debug=select_stm+rep_with_value(where,data)
             cur = mysql.connection.cursor()
             data = tuple(data)
-            cur.execute(select_stm,data)
+            cur.execute(select_stm+where,data)
             table = cur.fetchall()
             cur1 = mysql.connection.cursor()
             cur1.execute('''select distinct year from Games order by year''')
             years=cur1.fetchall()
             years=[items[0] for items in years]
-            return render_template("players.html", debug=debug,table=table, years=years, year=int(year), lgID=lgID, tmID=tmID)
+            pname_drop=get_dropoff('select distinct pname from player_team')
+            teamdrop=get_dropoff('select distinct name from Team')
+            try:
+                year=int(year)
+            except:
+                year=''
+            return render_template("players.html", debug=debug,table=table, years=years, year=year, lgID=lgID, tmID=tmID, pname_drop=pname_drop,teamdrop=teamdrop)
 
         else:
-            select_stm = "SELECT * FROM player_team WHERE Syear =1972"
+            select_stm = "SELECT * FROM player_team WHERE Syear =1927"
             cur = mysql.connection.cursor()
             cur.execute(select_stm)
             table = cur.fetchall()
@@ -173,7 +216,9 @@ def players():
             cur1.execute('''select distinct year from Games order by year''')
             years=cur1.fetchall()
             years=[year[0] for year in years]
-            return render_template("players.html", table=table, years=years, year=1972, lgID='NHL', tmID='')    
+            pname_drop=get_dropoff('select distinct pname from player_team')
+            teamdrop=get_dropoff('select distinct name from Team')
+            return render_template("players.html", table=table, years=years, year=1927, lgID='NHL', tmID='',pname_drop=pname_drop,teamdrop=teamdrop)    
     except Exception as e:
         debug=e
         return render_template("players.html", debug=debug)
